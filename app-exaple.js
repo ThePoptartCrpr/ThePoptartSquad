5bc7e9bb867012a7fdd94c6f8309f52e532273b7
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2017-05-09 18:05:54 -0700

    Added example app.js

---
+++ app-exaple.js
@@ -0,0 +1,10 @@
+const komada = require("komada");
+
+komada.start({
+  botToken: "<token redacted>",
+  ownerID: "198466968725094400",
+  clientID: "307650709275541505",
+  prefix: "p!",
+  clientOptions: {
+  },
+});
